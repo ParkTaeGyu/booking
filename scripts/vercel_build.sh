348dcaa2d7043ddb52e00fbd305f6cd7876b106f
ParkTaeKyu <taekyu2015@gmail.com>
2026-02-03 16:18:07 +0900

    수정: Vercel 빌드에서 Python으로 Flutter 설치

--- scripts/vercel_build.sh
+++ scripts/vercel_build.sh
@@ -4,9 +4,16 @@ set -eu
 FLUTTER_VERSION="3.38.7"
 ARCHIVE_URL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
 
-curl -fsSL "$ARCHIVE_URL" -o flutter.tar.xz
+python3 - <<PY
+import tarfile
+import urllib.request
 
-tar -xf flutter.tar.xz
+url = "${ARCHIVE_URL}"
+archive = "flutter.tar.xz"
+urllib.request.urlretrieve(url, archive)
+with tarfile.open(archive) as tar:
+    tar.extractall()
+PY
 
 export PATH="$PWD/flutter/bin:$PATH"
 
