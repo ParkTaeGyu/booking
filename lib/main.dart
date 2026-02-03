7ba126aa20905da869c39b78ffeb054f18674bee
ParkTaeKyu <taekyu2015@gmail.com>
2026-02-03 15:07:32 +0900

    수정: 웹에서 읽을 수 있는 env 경로로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'ui/pages/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: 'assets/config.env');
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL'] ?? '',
     anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
